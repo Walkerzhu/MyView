698f804f0a5130b04c22df5408c56ebf3fa6a125
zhuchangwei <zhuchangwei@agliesc.com.cn>
2016-11-18 09:45:13 +0800

    新建文件

---
+++ MYView/MYView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYView
+//
+//  Created by asc-mac1 on 2016/11/18.
+//  Copyright © 2016年 Asc.inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYView/MYView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYView
+//
+//  Created by asc-mac1 on 2016/11/18.
+//  Copyright © 2016年 Asc.inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
